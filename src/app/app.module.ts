286a6bbc96f9aede1f86a025a1971053eb1dfd3f
Andreia de Freitas Novais <andreiafreitasn@gmail.com>
2023-01-12 15:55:15 -0300

    versao final do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    Error404Component,
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {path: '', redirectTo: 'courses', pathMatch: 'full'},
-      {path: '**', component: Error404Component}
+      
       
     ])
   ],
