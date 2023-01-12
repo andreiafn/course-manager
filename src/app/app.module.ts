1621afb294dd83f876a3d1d91214f28174d11e1a
Andreia de Freitas Novais <andreiafreitasn@gmail.com>
2023-01-12 13:35:42 -0300

    adicionando a pasta share com pipe e replace modules separados e o botao de deletar curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { CourseListComponent } from './courses/course-list.component';
 import { RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
-import { StarComponent } from './star/star.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component,
-    StarComponent,
-    CourseInfoComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    FormsModule,
+    CourseModule,
     RouterModule.forRoot([
-      {path: 'courses/info/:id', component: CourseInfoComponent},
       {path: '', redirectTo: 'courses', pathMatch: 'full'},
-      {path: 'courses', component: CourseListComponent},
       {path: '**', component: Error404Component}
       
     ])
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,28 +1,51 @@
-import { Component, OnInit } from "@angular/core";
-import { Course } from "./course";
-import { CourseService } from "./course.service";
-@Component({
-    templateUrl: './course-list.component.html',
-})
-export class CourseListComponent implements OnInit {
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+import { CourseService } from './course.service';
 
-    _filterBy: string;
-    _courses: Course[] = [];
+@Component({
+    templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit { 
+
     filteredCourses: Course[] = [];
 
-    constructor(private courseService: CourseService) { }
+    _courses: Course[] = [];
     
-    ngOnInit(): void {
-        this._courses = this.courseService.retrieveAll();
-        this.filteredCourses = this._courses;
+    _filterBy: string;
+
+    constructor(private courseService: CourseService) { }
+
+    ngOnInit(): void { 
+        this.retrieveAll();
     }
 
-    set filter(value:string){
-        this._filterBy = value;
-        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLowerCase().indexOf(this._filterBy.toLowerCase()) > -1);
-     }
+    retrieveAll(): void { 
+        this.courseService.retrieveAll().subscribe({
+            next: courses => {
+                this._courses = courses;
+                this.filteredCourses = this._courses;
+            },
+            error: err => console.log('Error', err) 
+        })
+    }
 
-     get filter(){
+    deleteById(courseId: number): void { 
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => { 
+                console.log('Deleted with success');
+                this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        })
+    }
+
+    set filter(value: string) { 
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter() { 
         return this._filterBy;
-   }
+    }
 }
